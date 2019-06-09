abf3d725bef8ebefea0486fab67b2bd9f05c5117
EliasAras <elias.aras@edu.ece.fr>
2019-06-09 15:37:46 +0200

    ComboBox

--- ING3_Projet_Java/src/controleur/recherche.java
+++ ING3_Projet_Java/src/controleur/recherche.java
@@ -6,6 +6,8 @@
 package controleur;
 import java.sql.*;
 import ConnectionBDD.*;
+import static ConnectionBDD.ConnectMySQL.*;
+import java.util.ArrayList;
 import modele.*;
 import vue.*;
 
@@ -15,7 +17,45 @@ import vue.*;
  * @author elias
  */
 public class recherche {
-    public void rechercher(ConnectMySQL s){
+    public void main(String[] args){
+            ConnectMySQL m=new ConnectMySQL();
+            m.setCnx(connecterDB("localhost", "8889","Ecole", "root", "root"));
+            
+            InfoClasse(m, "2018", "Ing 3");
+    }
+    
+    public void InfoClasse(ConnectMySQL s, String Annee, String Niveau){
+        ArrayList<Object> result_Annee= new ArrayList<>();
+        ArrayList<Object> result_Niv= new ArrayList<>();
+        ArrayList<Object> result_Clas= new ArrayList<>();
+        ArrayList<Classe> Bon_result= new ArrayList<>();
         
+        result_Annee = s.rechercher("AnneeDebut", Annee, "AnneeScolaire");
+        result_Niv = s.rechercher("Nom", Niveau, "Niveau");
+        
+        if(!result_Annee.isEmpty() && !result_Niv.isEmpty()){
+            
+           AnneeScolaire a = ((AnneeScolaire)result_Annee.get(0));
+           
+           Niveau n = ((Niveau)result_Niv.get(0));
+           
+           result_Clas = s.rechercher("idAnneeScolaire", Integer.toString(a.getId()), "Classe");
+           
+           if(!result_Clas.isEmpty()){
+               for(int i=0; i < result_Clas.size(); i++){
+                   if( ((Classe)result_Clas.get(i)).getIdNiveau() == n.getId()){
+                       Bon_result.add(((Classe)result_Clas.get(i)));
+                   }
+               }
+           }
+           
+           if(!Bon_result.isEmpty()){
+               for(Classe c : Bon_result){
+                   System.out.println(c.toString());
+               }
+           }else{
+               System.out.println("");
+           }
+        }
     }
 }
